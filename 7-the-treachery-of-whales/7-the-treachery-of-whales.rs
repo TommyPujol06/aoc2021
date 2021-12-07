a16697200ce242e0925ed4bf7b3663df48545809
Tommy Pujol <git@itstommy.xyz>
2021-12-07 22:04:53 +0100

    Add part 1 of day 7.

---
+++ 7-the-treachery-of-whales/7-the-treachery-of-whales.rs
@@ -0,0 +1,31 @@
+fn abs(n: isize) -> isize {
+    if n > 0 {
+        n
+    } else {
+        n * -1
+    }
+}
+
+fn calculate_cheapest_move(input: Vec<isize>) -> isize {
+    let mut costs: Vec<isize> = Vec::new();
+    let max_pos = input.iter().max().unwrap();
+    for pos in 0..max_pos + 1 {
+        let mut total_fuel = 0;
+        for i in input.iter() {
+            total_fuel += abs(i - pos);
+        }
+        costs.push(total_fuel);
+    }
+
+    *costs.iter().min().unwrap()
+}
+
+fn main() {
+    let input = include_str!("7-the-treachery-of-whales.txt")
+        .trim()
+        .split(",")
+        .map(|x| x.to_string().parse::<isize>().unwrap())
+        .collect();
+
+    println!("p1: {}", calculate_cheapest_move(input));
+}
